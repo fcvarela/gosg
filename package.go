e6876d12cff29bac42dac24c01175cc16e3b8a60
Filipe Varela <fcvarela@gmail.com>
2016-11-11 19:28:00 +0000

    Edited package doc

--- package.go
+++ package.go
@@ -7,10 +7,4 @@
 // This package is experimental and expect it to break at any time. It will
 // also change dramatically when it moves to zero-dependency subpackages and
 // separate scenegraph + submission packages.
-//
-// Contributing
-//
-// Contributions in the form of features, improvements or even architectural
-// changes are most welcome and should be submitted in the form of
-// pull requests following a discussion ticket/issue.
 package gosg
